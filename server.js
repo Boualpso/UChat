fe4f83ffdb95e1ff527e9be31683f4a4db165ce2
Boualpso <cmtmoussa@gmail.com>
2023-08-11 22:25:55 +0300

    Tweak with web socket

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
+
+//Run when client connects
+io.on('connection',socket => {
+    
+    //Broadcast only to said user
+    socket.emit('message', 'Welcome to UChat!');
+
+    //Broadcast when a user connectsto everyone except said user 
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //Runs when clien disconnects 
+    socket.on('disconnect', () => {
+        //Broadcast to everyone 
+        io.emit('message', 'A user has left the chat');
+    });
+
+    //Listen for chat message
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    })
+});
+
 const PORT = 3000 || process.nextTick.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
